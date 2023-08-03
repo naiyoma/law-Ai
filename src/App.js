9bc60e69dfdbe828efa779f23c6c566c2ce4e329
naiyoma <lankas.aurelia@gmail.com>
2023-08-03 23:03:03 +0300

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./components/Navbar";
+import './index.css';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
+}
+
+export default App;
