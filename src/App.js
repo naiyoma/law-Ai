5e6b3255cd1196b6fa0d756508028c3fd447e5c1
naiyoma <lankas.aurelia@gmail.com>
2023-08-10 23:42:31 +0300

    info cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './Home';
-import './index.css';
-
+// import Home from './Home';
+// import './index.css';
+import infoCards from './components/inforCards';
 
 function App() {
   return (
     <>
-    <Home />
+    <infoCards />
     </>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import Navbar from "./components/Navbar";
-import './index.css';
+// import React from 'react';
+// import Navbar from "./components/Navbar";
+// import './index.css';
+// import { TERipple } from 'tw-elements-react';
 
+// function Home() {
+//   return (
+//     <>
+//     <Navbar/>
+//     <div class="grid grid-rows-4 grid-flow-col gap-1">
+//       <div
+//       class="bg-cover "
+//       style={{
+//           backgroundImage: `url(${require('./assets/coverimage.jpg')})`,
+//           height: '500px', // Adjust the height value as needed
+//         }}
+//         >
+//         <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed">
+//           <div class="flex h-full items-center justify-start">
+//             <div class="text-white">
+//               <h2 class="mb-6 text-4xl font-semibold">We are UoN Law</h2>
+//               <h4 class="mb-6 text-xl font-semibold">Creators and Innovators</h4>
+//             </div>
+//         </div>
+//     </div>
+//   </div>
+//     <div>
+//       <div
+//         class="block max-w-sm rounded-lg bg-white bg-cover p-6 shadow-lg dark:bg-neutral-700"
+//         style="background-image: url('...');">
+//         <h5
+//           class="mb-2 text-xl font-medium leading-tight text-neutral-800 text-white dark:text-neutral-50">
+//           Card title
+//         </h5>
+//         <p
+//           class="mb-4 text-base text-neutral-600 text-white  dark:text-neutral-200">
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </p>
+//         <button
+//           type="button"
+//           class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+//           data-te-ripple-init>
+//           Button
+//         </button>
+//       </div>
+//     </div>
+//     </div>
+//     </>
+//   );
+// }
 
-function Home() {
-  return (
-    <>
-    <Navbar/>
-    <div class="grid grid-rows-4 grid-flow-col gap-1">
-    <div
-    class="bg-cover"
-    style={{ backgroundImage: `url(${require('./assets/coverimage.jpg')})` }}
-  >
-    01
-  </div>
-    <div>02</div>
-  
-   
-    </div>
-    </>
-  );
-}
-
-export default Home;
+// export default Home;
