9faf02a89d2f69a3aa96bab9e9884144eaca6b9d
naiyoma <lankas.aurelia@gmail.com>
2023-08-24 16:24:19 +0300

    adding guidling

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -4,11 +4,22 @@ import { useState } from "react";
 
 export default function Guide() {
     return ( 
-        <div>
-            <div className="grid grid-rows-2 ">
-                <div className="bg-cyan-800">01</div>
-                <div className="bg-fuchsia-500">02</div>
-            </div>
-        </div>
+    
+<div class="grid grid-rows-2">
+    <div class="bg-cyan-800">01</div>
+    <div class="flex flex-row mx-4 md:mx-14 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <ol class="list-decimal pl-4 pr-4">
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        <li>Formation and Membership: Establish the Tech AI Law Network as a recognized organization, open to students and professionals from diverse disciplines, inviting membership from Kenyan universities and beyond.</li>
+        </ol>
+    </div>
+    <div class="bg-white"></div>
+</div>
     );
 }
