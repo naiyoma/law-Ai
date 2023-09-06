e8416b99c5265436991c528f71d4f574f3c4aa1c
naiyoma <lankas.aurelia@gmail.com>
2023-09-06 22:28:23 +0300

    responsive modal

--- src/components/ImageCover.js
+++ src/components/ImageCover.js
@@ -31,61 +31,82 @@ export default function HomePage() {
           <div className="md:flex md:items-center md:justify-center p-4">
             <img src={AboutSvg} alt="Logo" className="max-w-full h-auto" />
           </div>
-      {showModal ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h4 className="text-xl font-semibold">
-                  Objectives:
-                  </h4>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
+          {showModal ? (
+  <>
+    {/* Background overlay */}
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-opacity-50 bg-black">
+      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          {/* Modal header */}
+          <div className="flex items-center justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+            <h4 className="text-xl font-semibold text-gray-500">
+              Objectives:
+            </h4>
+            <button
+                  type="button"
+                  class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="authentication-modal"
+                  onClick={() => setShowModal(false)}
+                >
+                  <svg
+                    class="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-              <p class="mb-3 text-gray-500 dark:text-gray-400">1. Awareness and Education: Raise awareness among students and professionals about the legal implications and challenges posed by technology,
-              artificial intelligence, and machine learning through educational resources, workshops, seminars, and lectures.</p>
-              <p class="text-gray-500 dark:text-gray-400">2. Collaboration and Networking: Connect students, industry professionals, legal experts, and scholars to foster cross-disciplinary collaboration,
-              meaningful discussions, and exchange of ideas on technology and AI law.</p>
-              <p class="mb-3 text-gray-500 dark:text-gray-400">3. Knowledge Sharing and Research: Become a central repository of knowledge and resources related to technology, artificial intelligence, and the
-              law in Africa. Promote knowledge sharing through publications, research projects, and conferences/symposiums to contribute to the development
-              of legal frameworks.</p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+                </button>
+          </div>
+
+          {/* Modal body */}
+          <div className="relative p-6 flex-auto">
+            <p className="mb-3 text-gray-500 dark:text-gray-400">
+              1. Awareness and Education: Raise awareness among students and professionals about the legal implications and challenges posed by technology,
+              artificial intelligence, and machine learning through educational resources, workshops, seminars, and lectures.
+            </p>
+            <p className="mb-3 text-gray-500 dark:text-gray-400">
+            2. Collaboration and Networking: Connect students, industry professionals, legal experts, and scholars to foster cross-disciplinary collaboration,
+            meaningful discussions, and exchange of ideas on technology and AI law.
+            </p>
+            <p className="mb-3 text-gray-500 dark:text-gray-400">
+            3. Knowledge Sharing and Research: Become a central repository of knowledge and resources related to technology, artificial intelligence, and the
+            law in Africa. Promote knowledge sharing through publications, research projects, and conferences/symposiums to contribute to the development
+            of legal frameworks.
+            </p>
+            {/* Add more content here */}
+          </div>
+
+          {/* Focus Area */}
+          <div className="p-5 border-b border-solid border-slate-200 rounded-t">
+            <h4 className="text-xl font-semibold text-gray-500">Focus Area:</h4>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
+              <div className="flex items-center text-gray-500">
+                <span className="mr-2">&#8226;</span>
+                Privacy laws relating to tech
+                <span className="mr-2">&#8226;</span>
+                Privacy laws relating to tech
               </div>
+              {/* Add more focus areas here */}
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+        </div>
+      </div>
+    </div>
+
+    {/* Background overlay */}
+    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+  </>
+) : null}
+
         </div>
       </div>
     );
